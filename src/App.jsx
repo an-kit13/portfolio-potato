03769aee545d2ad5846995c6df4cf93cc271d57a
an-kit13 <ankitkrsingh48@gmail.com>
2025-04-15 22:43:22 +0530

    contact section completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar";
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <FeatureCards />
       <ExperienceSection />
       <TechStack />
+      <Contact />
     </>
   );
 };
