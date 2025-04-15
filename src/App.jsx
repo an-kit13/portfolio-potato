8228520ebe180d154489cc9fbc7c900b02402083
an-kit13 <ankitkrsingh48@gmail.com>
2025-04-15 21:25:54 +0530

    tech stack section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
+import ExperienceSection from "./sections/ExperienceSection";
+import TechStack from "./sections/TechStack";
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <FeatureCards />
+      <ExperienceSection />
+      <TechStack />
     </>
   );
 };
